5f1a4bc4b7ba6c8fbddf0d929eadce95c94586c5
Anthony_ <anthonygiuliano59@gmail.com>
2022-09-28 00:35:13 +0200

    Initial Commit

---
+++ glfwParte1/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  glfwParte1
+//
+//  Created by Anthony Gerardo Giuliano on 28/09/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
