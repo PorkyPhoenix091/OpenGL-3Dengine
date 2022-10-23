2b3cd35a3e22bf029b86354f2a622e595eb82f51
Anthony Gerardo Giuliano <anthonygiuliano@MacBook-Pro-di-Anthony.local>
2022-10-23 16:35:01 +0200

    went on with following opengl tutorials till excersises

--- glfwParte1/main.cpp
+++ glfwParte1/main.cpp
@@ -1,14 +1,206 @@
-//
-//  main.cpp
-//  glfwParte1
-//
-//  Created by Anthony Gerardo Giuliano on 28/09/22.
-//
-
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
 #include <iostream>
+#include <cmath>
+void framebuffer_size_callback(GLFWwindow* window, int width, int height){
+        glViewport(0,0,width,height);
+    }
+void processInput(GLFWwindow* window){
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+        glfwSetWindowShouldClose(window, true);
+    }
+}
+const char *vertexShaderSource = "#version 330 core\n"
+        "layout (location = 0) in vec3 aPos;\n"
+        "void main()\n"
+        "{\n"
+        "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+        "}\0";
+const char *fragmentShaderSource = "#version 330 core\n"
+        "out vec4 FragColor;\n"
+        "uniform vec4 commieColor;\n"
+        "void main()\n"
+        "{\n"
+        "    FragColor = commieColor;\n"
+        "}\0";
+const char *fragmentShaderSourceYellow = "#version 330 core\n"
+        "out vec4 FragColor;\n"
+        "void main()\n"
+        "{\n"
+        "    FragColor = vec4(1.0f, 1.0f, 0.0f, 1.0f);\n"
+        "}\0";
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+
+
+int main()
+{
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    
+    GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+    if(window == NULL){
+        std::cout<<"Window failed to load";
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+    glViewport(0,0,800,600);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+    
+    //creazione dello shader
+    unsigned int vertexShader;
+    vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1,&vertexShaderSource,NULL);
+    glCompileShader(vertexShader);
+    
+    ///
+    int  success;
+    char infoLog[512];
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+    if(!success)
+    {
+        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
+    ///
+    //creazione del fragment shader(per il colore)
+    
+    unsigned int fragmentShader;
+    fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource,NULL);
+    glCompileShader(fragmentShader);
+    
+    unsigned int fragmentShaderYellow;
+    fragmentShaderYellow = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShaderYellow, 1,&fragmentShaderSourceYellow,NULL);
+    glCompileShader(fragmentShaderYellow);
+    
+    ///
+    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+    if(!success)
+    {
+        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
+    glGetShaderiv(fragmentShaderYellow, GL_COMPILE_STATUS, &success);
+    if(!success)
+    {
+        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
+    ///
+    //creazione del programma shader
+    unsigned int shaderProgram;
+    shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram,vertexShader);
+    glAttachShader(shaderProgram,fragmentShader);
+    glLinkProgram(shaderProgram);
+    
+    unsigned int shaderProgramYell;
+    shaderProgramYell = glCreateProgram();
+    glAttachShader(shaderProgramYell, vertexShader);
+    glAttachShader(shaderProgramYell,fragmentShaderYellow);
+    glLinkProgram(shaderProgramYell);
+    
+    ///
+    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+    if(!success) {
+        glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::PROGRAM::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
+    glGetProgramiv(shaderProgramYell, GL_LINK_STATUS, &success);
+    if(!success) {
+        glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+    }
+    ///
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+    glDeleteShader(fragmentShaderYellow);
+    
+    
+    
+    float vertices[]{
+        0.0f,  0.5f, 0.0f,  // top right
+        0.5f, -0.5f, 0.0f,  // bottom right
+        -0.5f, -0.5f, 0.0f,  // bottom left
+        
+    };
+    float vertices2[]{
+        -0.6f, -0.5f, 0.0f,
+        -0.9f, 0.5f, 0.0f,
+        -0.1f, 0.5f, 0.0f
+    };
+//    unsigned int indices[] = {
+//        0,1,3,
+//        1,2,3
+//    };
+    //linguaggio glsl per la programmazione degli shader
+    
+    
+    //creazione del buffer ed assegnazione dei vertici al buf
+    unsigned int VBO,VAO;
+    //unsigned int EBO;
+    glGenVertexArrays(1, &VAO);
+    glBindVertexArray(VAO);
+    glGenBuffers(1, &VBO);
+    //creazione di buffer(EBO serve per l'ottimizzazione dei vertici
+    
+    //glGenBuffers(1,&EBO);
+    
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    //glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    //glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices),indices,GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER,sizeof(vertices),vertices,GL_STATIC_DRAW);
+
+    
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float),(void*)0);
+    glEnableVertexAttribArray(0);
+    
+    unsigned int VBO2, VAO2;
+    glGenVertexArrays(1, &VAO2);
+    glBindVertexArray(VAO2);
+    glGenBuffers(1,&VBO2);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO2);
+    
+    glBufferData(GL_ARRAY_BUFFER,sizeof(vertices2),vertices2,GL_STATIC_DRAW);
+    
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float),(void*)0);
+    glEnableVertexAttribArray(0);
+
+    //serve ad attivare la modalità wireframe
+    glPolygonMode(GL_FRONT_AND_BACK,GL_FILL);
+
+    
+    
+    while (!glfwWindowShouldClose(window)) {
+        processInput(window);
+        
+
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+        glUseProgram(shaderProgram);
+        
+        float timeValue = glfwGetTime();
+        float greenValue = (sin(timeValue) / 2.0f) + 0.5f;
+        int vertexColorLocation = glGetUniformLocation(shaderProgram,"commieColor");
+        glUniform4f(vertexColorLocation,0.0f,greenValue,0.0f,1.0f);
+        
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLES, 0, 6);
+        glUseProgram(shaderProgramYell);
+        glBindVertexArray(VAO2);
+        glDrawArrays(GL_TRIANGLES, 0, 6);
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+    glfwTerminate();
     return 0;
 }
